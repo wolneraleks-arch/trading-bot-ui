5c9991eb828f434e8e96a099bc8980db9ad1bcec
wolneraleks-arch <wolneraleks@gmail.com>
2026-02-24 04:10:42 +0300

    Update encoder.js

--- encoder.js
+++ encoder.js
@@ -1,21 +1,16 @@
-// GitHub: encoder.js
-const VK_ENCODER = {
-    // Наша секретная карта (можешь менять буквы на любые другие)
-    {
-  "k": "z", "m": "k", "a": "m", "p": "a", "q": "p", "s": "q", "w": "s", "e": "w", "r": "e", "t": "r",
+{
+  "0": "z",
+  "1": "k",
+  "2": "m",
+  "3": "a",
+  "4": "p",
+  "5": "q",
+  "6": "s",
+  "7": "w",
+  "8": "e",
+  "9": "r",
   "prefix": "wi_",
   "suffix": "_v"
 }
 
-
-    encode: function(vkId) {
-        if (!vkId) return "";
-        let encoded = this.prefix;
-        let idStr = vkId.toString();
-        for (let char of idStr) {
-            encoded += this.map[char] || char;
-        }
-        return encoded + this.suffix;
-    }
-};
 
